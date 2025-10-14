04db1f0c542c0a0b9711007e2c3f7cb1ed7ceffb
mhlebens <marifermatah@gmail.com>
2025-10-13 22:29:12 -0600

    Implemented Product CRUD with views and business logic

---
+++ Pyme.Abstracciones/AccesoADatos/Producto/ObtenerProductoPorId/IObtenerProductoPorIdAD.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pyme.Abstracciones.AccesoADatos.Producto.ObtenerProductoPorId
+{
+    internal class IObtenerProductoPorIdAD
+    {
+    }
+}
