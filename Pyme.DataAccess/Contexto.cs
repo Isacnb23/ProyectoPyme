297a61cfc6da7695570629f03321e9fcefc137f3
mhlebens <marifermatah@gmail.com>
2025-10-14 17:08:52 -0600

    Update Contexto and App.config for EF6 and database connection setup

---
+++ Pyme.DataAccess/Contexto.cs
@@ -0,0 +1,16 @@
+﻿using Pyme.DataAccess.Modelos;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Runtime.Remoting.Contexts;
+
+namespace Pyme.AccesoADatos
+{
+    public class Contexto : DbContext
+    {
+        public Contexto() : base("name=Pyme")
+        {
+        }
+
+        public DbSet<ProductoAD> Producto { get; set; }
+    }
+}
