597eb3142cdd603685575278149f03d0a22a951b
URANO000 <kkonigin1100@gmail.com>
2025-10-13 21:10:58 -0600

    Most shared views done

--- PymeCo/obj/Debug/net9.0/PymeCo.AssemblyInfo.cs
+++ PymeCo/obj/Debug/net9.0/PymeCo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PymeCo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efe2002a8fa954395bd98067c55df49217e733d9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ec3acc8b78193554047b0e2735f5de1fbfd6afc")]
 [assembly: System.Reflection.AssemblyProductAttribute("PymeCo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PymeCo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
