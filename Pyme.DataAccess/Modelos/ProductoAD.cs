c850a39c02bfc7438f37f17b62336464c272615e
mhlebens <marifermatah@gmail.com>
2025-10-14 07:55:26 -0600

    Add ProductoAD model aligned with Producto table structure

--- Pyme.DataAccess/Modelos/ProductoAD.cs
+++ Pyme.DataAccess/Modelos/ProductoAD.cs
@@ -3,10 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Pyme.DataAccess.Modelos
 {
-    internal class ProductoAD
+    [Table("Producto", Schema = "dbo")]
+    public class ProductoAD
     {
+        [Column("Id")]
+        public int Id { get; set; }
+
+        [Column("Nombre")]
+        public string Nombre { get; set; }
+
+        [Column("CategoriaId")]
+        public int CategoriaId { get; set; }
+
+        [Column("Precio")]
+        public decimal Precio { get; set; }
+
+        [Column("ImpuestoPorc")]
+        public decimal ImpuestoPorc { get; set; }
+
+        [Column("Stock")]
+        public int Stock { get; set; }
+
+        [Column("ImagenUrl")]
+        public string ImagenUrl { get; set; }
+
+        [Column("EstadoProducto")]
+        public string EstadoProducto { get; set; }
     }
 }
